6d84e7aa43aef845b3fc9da1257c1e36867c77af
BEKKOUCH Imad Eddine Ibrahim <i.bekkouch@innopolis.university>
2019-02-07 22:45:20 +0300

    Homework2 read images and labeling

--- 3-Face-Recognition/main.py
+++ 3-Face-Recognition/main.py
@@ -1,8 +1,22 @@
 import argparse
+import os
+import cv2
+import glob
 
 
 def main(config):
-    pass
+    names = os.listdir(config.dataset)
+    print("nb of people:", len(names))
+    names2label = {name: label for name, label in zip(names, range(len(names)))}
+    print(names2label)
+
+    im_size = 299
+    load_image = lambda im_path: cv2.resize(cv2.imread(im_path), (im_size, im_size))
+
+    for name in names:
+        for path in glob.glob(config.dataset+"/"+name+"/*.jpg"):
+            img = load_image(path)
+            print(img.shape, name)
 
 
 if __name__ == '__main__':
