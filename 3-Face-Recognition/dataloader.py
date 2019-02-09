ab88dcb578567a465c89af59b962868708433ced
BEKKOUCH Imad Eddine Ibrahim <i.bekkouch@innopolis.university>
2019-02-09 15:13:21 +0300

    code refactoring

---
+++ 3-Face-Recognition/dataloader.py
@@ -0,0 +1,31 @@
+import argparse
+import os
+import cv2
+import glob
+import numpy as np
+from model import  Model
+
+class DataLoader:
+    def __init__(self,dataset):
+        self.dataset = dataset
+        self.names = os.listdir(self.dataset)
+        # print("nb of people:", len(self.names))
+        self.names2label = {name: label for name, label in zip(self.names, range(len(self.names)))}
+        # print(self.names2label)
+    def get_latent(self,weights):
+        model = Model(weights)
+        im_size = 299
+        load_image = lambda im_path: cv2.resize(cv2.imread(im_path), (im_size, im_size))
+        latent_space = []
+        print("[INFO] generate latent representations of images")
+        progress = 0
+        for name in self.names:
+            print("progress %d/%d" % (progress, len(self.names)))
+            progress += 1
+            for path in glob.glob(self.dataset + "/" + name + "/*.jpg"):
+                img = load_image(path)
+                latent = model.get_latent_space(img)
+                latent_space.append([latent.flatten(), self.names2label[name]])
+            # if progress > 10:
+            #     break
+        return np.array(latent_space)
--- 3-Face-Recognition/main.py
+++ 3-Face-Recognition/main.py
@@ -3,29 +3,13 @@ import os
 import cv2
 import glob
 import numpy as np
+
+from dataloader import DataLoader
 from model import  Model
 
 def main(config):
-    names = os.listdir(config.dataset)
-    # print("nb of people:", len(names))
-    names2label = {name: label for name, label in zip(names, range(len(names)))}
-    # print(names2label)
-    model = Model(config.weights)
-    im_size = 299
-    load_image = lambda im_path: cv2.resize(cv2.imread(im_path), (im_size, im_size))
-    latent_space = []
-    print("[INFO] generate latent representations of images")
-    progress = 0
-    for name in names:
-        print("progress %d/%d" % (progress, len(names)))
-        progress += 1
-        for path in glob.glob(config.dataset + "/" + name + "/*.jpg"):
-            img = load_image(path)
-            latent = model.get_latent_space(img)
-            latent_space.append([latent.flatten(), names2label[name]])
-        if progress > 10:
-            break
-    latent_space = np.array(latent_space)
+    data_loader = DataLoader(config.dataset)
+    latent_space = data_loader.get_latent(config.weights)
     print(latent_space[0].shape)
     print(latent_space[0])
     print(latent_space.shape)
