f386455cadeca02f7f6fa2c82c221afafe74c5c6
BEKKOUCH Imad Eddine Ibrahim <i.bekkouch@innopolis.university>
2019-02-17 19:09:49 +0300

    Final Splines Homework

--- 3-splines/__init__.py
+++ 3-splines/__init__.py
@@ -1,63 +1,55 @@
 import pandas as pd
+from patsy import dmatrix
+import statsmodels.api as sm
+import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.datasets import load_boston
-boston = load_boston()
 from sklearn.metrics import mean_squared_error
 from math import sqrt
-bos = pd.DataFrame(boston.data)
+from sklearn.model_selection import train_test_split
 
+boston = load_boston()
+print(boston.DESCR)
+
+bos = pd.DataFrame(boston.data)
 bos.columns = boston.feature_names
 bos['PRICE'] = boston.target
 
 data_x = bos['CRIM']
 data_y = bos['PRICE']
 
-# Dividing data into train and validation datasets
-from sklearn.model_selection import train_test_split
-train_x, valid_x, train_y, valid_y = train_test_split(data_x, data_y, test_size=0.33, random_state = 1)
+train_x, valid_x, train_y, valid_y = train_test_split(data_x, data_y, test_size=0.33, random_state=1)
 
-from patsy import dmatrix
-import statsmodels.api as sm
-import statsmodels.formula.api as smf
-import matplotlib.pyplot as plt
+transformed_x = dmatrix("bs(train, knots=(2,25,60), degree=3, include_intercept=False)", {"train": train_x},
+                        return_type='dataframe')
 
-
-# Generating cubic spline with 3 knots at 25, 40 and 60
-transformed_x = dmatrix("bs(train, knots=(25,40,60), degree=3, include_intercept=False)", {"train": train_x},return_type='dataframe')
-
-# Fitting Generalised linear model on transformed dataset
 fit1 = sm.GLM(train_y, transformed_x).fit()
 
-# Generating cubic spline with 4 knots
-transformed_x2 = dmatrix("bs(train, knots=(25,40,50,65),degree =3, include_intercept=False)", {"train": train_x}, return_type='dataframe')
+transformed_x2 = dmatrix("bs(train, knots=(10,25,50,65),degree =3, include_intercept=False)", {"train": train_x},
+                         return_type='dataframe')
 
-# Fitting Generalised linear model on transformed dataset
 fit2 = sm.GLM(train_y, transformed_x2).fit()
 
-# Predictions on both splines
-pred1 = fit1.predict(dmatrix("bs(valid, knots=(25,40,60), include_intercept=False)", {"valid": valid_x}, return_type='dataframe'))
-pred2 = fit2.predict(dmatrix("bs(valid, knots=(25,40,50,65),degree =3, include_intercept=False)", {"valid": valid_x}, return_type='dataframe'))
+pred1 = fit1.predict(
+    dmatrix("bs(valid, knots=(2,25,60), include_intercept=False)", {"valid": valid_x}, return_type='dataframe'))
+pred2 = fit2.predict(dmatrix("bs(valid, knots=(10,25,50,65),degree =3, include_intercept=False)", {"valid": valid_x},
+                             return_type='dataframe'))
 
-# Calculating RMSE values
 rms1 = sqrt(mean_squared_error(valid_y, pred1))
 print(rms1)
 rms2 = sqrt(mean_squared_error(valid_y, pred2))
 print(rms2)
 
-# We will plot the graph for 70 observations only
-xp = np.linspace(valid_x.min(),valid_x.max(),70)
+xp = np.linspace(valid_x.min(), valid_x.max(), 70)
 
-# Make some predictions
-pred1 = fit1.predict(dmatrix("bs(xp, knots=(25,40,60), include_intercept=False)", {"xp": xp}, return_type='dataframe'))
-pred2 = fit2.predict(dmatrix("bs(xp, knots=(25,40,50,65),degree =3, include_intercept=False)", {"xp": xp}, return_type='dataframe'))
+pred1 = fit1.predict(dmatrix("bs(xp, knots=(2,25,60), include_intercept=False)", {"xp": xp}, return_type='dataframe'))
+pred2 = fit2.predict(
+    dmatrix("bs(xp, knots=(10,25,50,65),degree =3, include_intercept=False)", {"xp": xp}, return_type='dataframe'))
 
-# Plot the splines and error bands
 plt.scatter(data_x, data_y, facecolor='None', edgecolor='k', alpha=0.1)
-plt.plot(xp, pred1, label='Specifying degree =3 with 3 knots')
-plt.plot(xp, pred2, color='r', label='Specifying degree =3 with 4 knots')
+plt.plot(xp, pred1, label='3 knots')
+plt.plot(xp, pred2, color='r', label='4 knots')
 plt.legend()
-# plt.xlim(15,85)
-# plt.ylim(0,80)
-plt.xlabel('age')
-plt.ylabel('wage')
+plt.xlabel('per capita crime rate by town')
+plt.ylabel('Price')
 plt.show()
