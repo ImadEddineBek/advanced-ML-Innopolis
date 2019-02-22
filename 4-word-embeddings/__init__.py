44ce207cbf4b1e4a8738b084ab7ec3f93341ce6e
BEKKOUCH Imad Eddine Ibrahim <i.bekkouch@innopolis.university>
2019-02-22 20:58:14 +0300

    Model

--- 4-word-embeddings/__init__.py
+++ 4-word-embeddings/__init__.py
@@ -1,5 +1,9 @@
 from collections import Counter
 import numpy as np
+from tensorflow.python.ops.nn_impl import sigmoid_cross_entropy_with_logits as sig
+import os
+import tensorflow as tf
+from tensorflow.contrib.tensorboard.plugins import projector
 
 
 class Vocabulary:
@@ -28,7 +32,7 @@ class Vocabulary:
     def get_neg_sample(self, context_size=5):
         return np.random.choice(range(len(self.words)), 2 * context_size, p=self.modified_unigram_weighs)
 
-    def get_next_batch(self, n_contexts=20, context_size=5, k=None):
+    def get_next_batch(self, n_contexts=200, context_size=5, k=None):
         batch = []
         for i in range(n_contexts):
             central, pos = self.get_next_context(context_size)
@@ -44,7 +48,7 @@ class Vocabulary:
         line = self.lines[self.line_index]
         center = line[self.in_line_index]
         center = self.word_to_int(center)
-        if center == -1:
+        if center != -1:
             beg_index = self.in_line_index - context_size if self.in_line_index - context_size >= 0 else 0
             end_index = self.in_line_index
             prev = line[beg_index:end_index]
@@ -82,20 +86,92 @@ class Vocabulary:
     def is_most_common(self, word):
         return word in self.words
 
+    def get_words(self):
+        return self.words
+
 
 class Model:
-    def __init__(self):
-        pass
+    def __init__(self, vocab, vocabulary_size=5000, embedding_size=50):
+        # self.W_IN = tf.get_variable("W_IN",[None,])
+        self.vocab = vocab
+        self.LOG_DIR = 'imad_logs'
+        if not os.path.exists(self.LOG_DIR):
+            os.makedirs(self.LOG_DIR)
+        self.metadata = os.path.join(self.LOG_DIR, 'metadata.tsv')
+        with open(self.metadata, 'w') as metadata_file:
+            for row in vocab.get_words():
+                metadata_file.write(str(row + "\n"))
+        self.sess = tf.Session()
+        with tf.name_scope('inputs'):
+            self.center = tf.placeholder(tf.int32, shape=[None, ], name="center")
+            self.word = tf.placeholder(tf.int32, shape=[None, ], name="word")
+            self.label = tf.placeholder(tf.float32, shape=[None, ], name="label")
+        with tf.name_scope('embeddings'):
+            self.W_IN = tf.Variable(
+                tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0), name="W_IN")
+            self.W_OUT = tf.Variable(
+                tf.random_uniform([vocabulary_size, embedding_size], -1.0, 1.0), name="W_OUT")
+            self.u = tf.nn.embedding_lookup(params=self.W_IN, ids=self.center,name='embeddings/W_IN')
+            self.v = tf.nn.embedding_lookup(params=self.W_OUT, ids=self.word,name='embeddings/W_OUT')
+            logits = tf.reduce_sum(self.u * self.v, axis=1)
 
-    def train(self):
-        pass
+        self.loss = tf.reduce_mean(sig(logits=logits, labels=self.label))
+        tf.summary.scalar('loss',self.loss)
+        # print(self.u, self.v)
+        # self.embedding_matrix = tf.nn.l2_normalize(0.5 * (self.W_IN + self.W_OUT), axis=1,name='W_IN_W_OUT')
+        # self.emb = tf.nn.embedding_lookup(params=self.embedding_matrix, ids=self.center)
 
+        # self.emb_var = tf.Variable(self.embedding_matrix, name='embeddings')
+
+        self.saver = tf.train.Saver()
+
+        self.optimizer = tf.train.AdamOptimizer(0.001).minimize(self.loss)
+        init = tf.global_variables_initializer()
+        self.sess.run(init)
+
+        writer = tf.summary.FileWriter('./graphs', self.sess.graph)
+        writer.close()
+        self.merged = tf.summary.merge_all()
+        self.train_writer = tf.summary.FileWriter(self.LOG_DIR ,
+                                             graph=self.sess.graph)
+
+    def train(self, epochs=10):
+        for i in range(epochs):
+            batch = self.vocab.get_next_batch()
+            center, words, labels = batch[:, 0], batch[:, 1], batch[:, 2]
+            _, loss,merged = self.sess.run(
+                [self.optimizer, self.loss,c],
+                feed_dict={self.center: center, self.word: words, self.label: labels})
+            print('loss', loss)
+            self.train_writer.add_summary(merged,i)
+
+        self.saver.save(self.sess, os.path.join(self.LOG_DIR, 'model.ckpt'))
+        config = projector.ProjectorConfig()
+        # One can add multiple embeddings.
+        embedding = config.embeddings.add()
+        embedding.tensor_name = 'embeddings/W_IN'
+        # Link this tensor to its metadata file (e.g. labels).
+        embedding.metadata_path = 'metadata.tsv'
+        embedding = config.embeddings.add()
+        embedding.tensor_name = 'embeddings/W_OUT'
+        # Link this tensor to its metadata file (e.g. labels).
+        embedding.metadata_path = 'metadata.tsv'
+        # # Saves a config file that TensorBoard will read during startup.
+
+        # embedding = config.embeddings.add()
+        # embedding.tensor_name = 'embeddings/W_IN_W_OUT'
+        # # Link this tensor to its metadata file (e.g. labels).
+        # embedding.metadata_path = 'metadata.tsv'
+
+        projector.visualize_embeddings(tf.summary.FileWriter(self.LOG_DIR), config)
     def log(self):
         pass
 
 
 def main():
-    vocab = Vocabulary("wikipedia_sample_tiny.txt", 50)
+    vocab = Vocabulary("wikipedia_sample_tiny.txt", 5000)
+    model = Model(vocab)
+    model.train()
 
 
 if __name__ == '__main__':
