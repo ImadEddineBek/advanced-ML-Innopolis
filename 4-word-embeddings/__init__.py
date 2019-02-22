948dafe06f3b04da47ab0604ae3b861cd4664080
BEKKOUCH Imad Eddine Ibrahim <i.bekkouch@innopolis.university>
2019-02-22 07:42:48 +0300

    negative sampling

--- 4-word-embeddings/__init__.py
+++ 4-word-embeddings/__init__.py
@@ -1,4 +1,5 @@
 from collections import Counter
+import numpy as np
 
 
 class Vocabulary:
@@ -8,32 +9,36 @@ class Vocabulary:
         lines = corpus_file.readlines()
         # while line != None:
         for line in lines:
-            #     for token in line.strip().split():
-            #         print(token)
             counter.update(line.split())
-            #
-            # line = corpus_file.readline()
-            # pass
-        # print(corpus_file.readline())
         most_freq = counter.most_common(n)
         self.words, self.frequencies = zip(*most_freq)
+        self.words, self.frequencies = np.array(self.words), np.array(self.frequencies)
         self.words_to_ints = {w: i for w, i in zip(self.words, range(len(self.words)))}
-        # print(self.words_to_ints)
-        # print(self.word_freq("the"))
+        unigram = self.frequencies / sum(self.frequencies)
 
-    def word_freq(self,word):
+        modified_unigram = np.power(unigram, 3 / 4)
+        modified_unigram_weighs = modified_unigram / sum(modified_unigram)
+
+        neg = np.random.choice(range(len(self.words)), 5, p=modified_unigram_weighs)
+        for n in neg:
+            print(self.int_to_word(n))
+
+    def word_freq(self, word):
         index = self.word_to_int(word)
         return self.frequencies[index]
 
     def generate_neg(self):
         pass
 
-    def word_to_int(self,word):
+    def word_to_int(self, word):
         return self.words_to_ints.get(word)
+
+    def int_to_word(self, i):
+        return self.words[i]
 
 
 def main():
-    vocab = Vocabulary("wikipedia_sample_tiny.txt", 5)
+    vocab = Vocabulary("wikipedia_sample_tiny.txt", 50)
 
 
 if __name__ == '__main__':
