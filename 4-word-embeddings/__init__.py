7c700b0790aefd1b26bb6d0a519e18f0f0828721
BEKKOUCH Imad Eddine Ibrahim <i.bekkouch@innopolis.university>
2019-02-22 09:08:10 +0300

    batch creation

--- 4-word-embeddings/__init__.py
+++ 4-word-embeddings/__init__.py
@@ -18,22 +18,42 @@ class Vocabulary:
 
         modified_unigram = np.power(unigram, 3 / 4)
         self.modified_unigram_weighs = modified_unigram / sum(modified_unigram)
+        self.line_index = 0
+        self.in_line_index = 0
 
     def word_freq(self, word):
         index = self.word_to_int(word)
         return self.frequencies[index]
 
     def get_neg_sample(self, context_size=5):
-        return np.random.choice(range(len(self.words)), context_size, p=self.modified_unigram_weighs)
+        return np.random.choice(range(len(self.words)), 2 * context_size, p=self.modified_unigram_weighs)
 
-    def generate_neg(self):
+    def get_next_batch(self, n_contexts=20, context_size=5, k=None):
+        batch = []
+        for i in range(n_contexts):
+            central, pos = self.get_next_context(context_size)
+            neg = self.get_neg_sample(context_size)
+            neg = np.delete(neg, np.argwhere(neg == central))
+            for p in pos:
+                batch.append([central, p, 1.0])
+            for n in neg:
+                batch.append([central, n, 0.0])
+        return np.array(batch)
+
+    def get_next_context(self, context_size):
+        # self.line
+        return None, [None]
+
+    def update_indecies(self):
         pass
-
     def word_to_int(self, word):
         return self.words_to_ints.get(word)
 
     def int_to_word(self, i):
         return self.words[i]
+
+    def is_most_common(self, word):
+        return word in self.words
 
 
 def main():
